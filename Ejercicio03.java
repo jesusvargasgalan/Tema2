2d2857aab31b32f9354a7fe9142b5bec869ec9bd
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-10-08 17:35:17 +0200

    Ejercicio03

---
+++ Ejercicio03.java
@@ -0,0 +1,30 @@
+/*
+ * Ejercicio03.java
+ * 
+ * Jesus Vargas Galan
+ * 
+ * 
+ *Crea las variables nombre, direccion y telefono y asígnale los valores corres-
+ *pondientes. Muestra los valores de esas variables por pantalla de tal forma
+ *que el resultado del programa sea el mismo que en el ejercicio 2.
+ * 
+ * 
+ */
+
+
+public class Ejercicio03 {
+
+  public static void main (String args[]) {
+    
+    
+    String nombre="Jesus";
+    String direccion="C/De las Palomas";
+    String numero="654678654";
+    
+    
+    System.out.println("Mi nombre es "+nombre+" vivo en la calle "+direccion+
+    " y mi numero es " + numero);
+
+  }
+}
+
