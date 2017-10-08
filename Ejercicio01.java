01b25af2ccba69c6072fb006b3822896ea710e74
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-10-08 17:26:04 +0200

    Ejercicio01

---
+++ Ejercicio01.java
@@ -0,0 +1,28 @@
+/*
+* Ejercicio01.java
+* 
+* Jesus Vargas Galan
+* 
+*Escribe un programa en el que se declaren las variables enteras x e y . Asignales
+*los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+*el valor de cada variable, la suma, la resta, la división y la multiplicación.
+*/
+
+
+public class Ejercicio01{
+	
+	public static void main (String args[]) {
+    
+    
+    int x=144;
+    int y=999;
+    
+    
+    System.out.println("X es igual a "+x+" e Y es igual a "+y);
+    System.out.println("Y+X es igual a "+(x + y));
+    System.out.println("Y-X es igual a "+(x - y));
+    System.out.println("Y*X es igual a "+(double)(x * y));
+		System.out.println("Y/X es igual a "+(float)(x / y));
+	}
+}
+
