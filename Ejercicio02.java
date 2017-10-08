03edccf0934b4495fd9441d0792388c0e5e5b940
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-10-08 17:35:08 +0200

    Ejercicio02

---
+++ Ejercicio02.java
@@ -0,0 +1,26 @@
+/*
+ * Ejercicio02.java
+ * 
+ * Jesus Vargas Galan
+ * 
+ *Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ *pantalla de tal forma que el resultado del programa sea el mismo que en el
+ *ejercicio 1 del capítulo 1.
+ * 
+ * 
+ */
+
+
+
+public class Ejercicio02 {
+
+  public static void main (String args[]) {
+    
+    
+    String nombre="Jesus";
+
+
+    System.out.println("Mi nombre es "+nombre);
+
+  }
+}
