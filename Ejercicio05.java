4899b2443b4c38297e97c56445b6f6dcf2b11e6d
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-10-08 17:35:32 +0200

    Ejercicio05

---
+++ Ejercicio05.java
@@ -0,0 +1,29 @@
+/*
+ * Ejercicio05.java
+ * 
+ * Jesus Vargas Galan
+ * 
+ *Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere
+ *convertir deberá estar almacenada en una variable.
+ * 
+ * 
+ */
+
+
+public class Ejercicio05{
+
+  public static void main (String args[]) {
+    
+    
+    int pesetas=4;
+    double conversor= pesetas / 166.386;
+    
+    
+    System.out.println(pesetas+" pesetas equivalen a " +conversor+ " euros");
+
+  }
+}
+
+
+
+
